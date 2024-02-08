2aaa9d190eb023637cfbef8e43af93f87e0dd69a
ddinhaz <100280510+ddinhaz@users.noreply.github.com>
2024-02-08 15:32:42 +0700

    Fixing Error

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject GameOver;
+
+    private Text GameOverText;
+
+    private void Start()
     {
-        
+        // Pastikan panel win dan lose dimatikan pada awalnya
+        GameOver.SetActive(false);
+
+        // Dapatkan komponen Text pada panel win dan lose
+        GameOverText = GameOver.GetComponentInChildren<Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayerCrossedFinishLine(GameObject player)
     {
-        
+        // Tampilkan panel win dan atur teks (jika diperlukan)
+        GameOver.SetActive(true);
+        GameOverText.text = "Hoki Kamu Dek!"; // Atur teks sesuai kebutuhan
+
+        // Lakukan tindakan lanjutan seperti menonaktifkan kontrol pemain atau menampilkan skor akhir
+        // ...
+    }
+
+    public void EnemyTouchesFinish()
+    {
+        // Tampilkan panel lose dan atur teks (jika diperlukan)
+        GameOver.SetActive(true);
+        GameOverText.text = "Cupu Banget Dek!"; // Atur teks sesuai kebutuhan
+
+        // Lakukan tindakan lanjutan seperti menonaktifkan kontrol pemain atau menampilkan pesan kekalahan
+        // ...
     }
 }
